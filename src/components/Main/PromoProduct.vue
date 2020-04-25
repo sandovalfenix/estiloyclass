cc4152e7238e7268fd092d38d4027fa3aecbbee3
Andres Felipe <sandovalfenix@hotmail.com>
2020-04-24 20:37:44 -0500

    cambio de flayer

--- src/components/Main/PromoProduct.vue
+++ src/components/Main/PromoProduct.vue
@@ -10,7 +10,7 @@
   <div class="col-12 col-md">
     <!-- Promo -->
     <a class="btn btn-bg-transparent transition-3d-hover px-5 mt-3" href="javascript:;">
-      <img class="img-thumbnail" src="https://firebasestorage.googleapis.com/v0/b/estiloyclass.appspot.com/o/banners%2FFLAYER%20%20version%202.webp?alt=media&token=a41a41ef-8029-44a2-9b36-dc875cc43ecf" alt="">
+      <img class="img-thumbnail" src="https://firebasestorage.googleapis.com/v0/b/estiloyclass.appspot.com/o/banners%2FBANNER%20CUADRADO%20compra%20tu%20tapaboca%20aqui.webp?alt=media&token=46d75deb-c249-4026-b78f-15129f10e0a8" alt="">
     </a>
     <!-- End Promo -->
   </div>
